02f509c5eb5fad8dcfd59861036c4b44dc03f52a
Dinesh P M <dineshpmd37@gmail.com>
2024-08-04 18:29:46 +0000

    commited the changes

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,4 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(materialized='incremental',
+unique_key ='id',
+incremental_strategy= "merge",
+tags=["staging"]) }}
 
 with stg_customers as (
     select * from {{source('staging','customers')}}
